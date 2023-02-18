02aa10e25c012cc6216bcc7dc6559e27b7721d30
Acy Watson <acy@fb.com>
2023-02-17 22:43:26 -0700

    semis

--- src/components/glyf-editor/Editor.stories.tsx
+++ src/components/glyf-editor/Editor.stories.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-import { type ComponentStory, type ComponentMeta } from '@storybook/react'
+import { type ComponentStory, type ComponentMeta } from '@storybook/react';
 
-import Editor from './Editor'
-import './styles.css'
-import './stories.css'
+import Editor from './Editor';
+import './styles.css';
+import './stories.css';
 
 // eslint-disable-next-line
 export default {
   title: 'Glyf Editor',
   component: Editor,
-} as ComponentMeta<typeof Editor>
+} as ComponentMeta<typeof Editor>;
 
-const Template: ComponentStory<typeof Editor> = (args) => <Editor {...args} />
+const Template: ComponentStory<typeof Editor> = (args) => <Editor {...args} />;
 
-export const FirstStory = Template.bind({})
+export const FirstStory = Template.bind({});
 
 FirstStory.args = {
   /* 👇 The args you need here will depend on your component */
-}
+};
